c86c505013069e6293bdce0cfd95085687877f85
Thalita <thalita.oabreu@outlook.com>
2024-03-03 19:04:10 -0300

    commit final

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.querySelectorAll()
+// função (comando que damos ao navegador para ele executar) que será executada para cada elemento:
+// estrutura da função:
+//  function nome (argumento) {
+//      conteúdo da função }   
+
+var elementosDuvida =  document.querySelectorAll(".duvida")
+
+elementosDuvida.forEach(function(duvida) {
+    duvida.addEventListener("click",function() {
+        duvida.classList.toggle("ativa")
+})
+})
